d3b27545057afa82f8c449210bf24b71d61bf82b
thaiskaroline <thaiskarolinebentosilva@hotmail.com>
2023-10-20 20:09:48 -0300

    versão 1

---
+++ src/main/java/my_first_api_web/exemplo/ExemploApplication.java
@@ -0,0 +1,13 @@
+package my_first_api_web.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my_first_api_web/exemplo/ExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package my_first_api_web.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
